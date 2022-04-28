5c157659e69c2ccafdb3fb8414ae05e7697dfcb6
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-04-28 16:22:02 -0500

    nuevas clases para repositiry

---
+++ apecs/src/main/java/com/apecs/factura/apecs/repository/Detalle_Repository.java
@@ -0,0 +1,5 @@
+package com.apecs.factura.apecs.repository;
+
+public class Detalle_Repository {
+    
+}
---
+++ apecs/src/main/java/com/apecs/factura/apecs/repository/Fatura_Repository.java
@@ -0,0 +1,5 @@
+package com.apecs.factura.apecs.repository;
+
+public class Fatura_Repository {
+    
+}
