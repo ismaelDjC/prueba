65551506897d16adbe0d06487db4f9127874bae7
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-04-28 16:06:34 -0500

    pruebas-campo

---
+++ apecs/src/main/java/com/apecs/factura/apecs/ApecsApplication.java
@@ -0,0 +1,13 @@
+package com.apecs.factura.apecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApecsApplication.class, args);
+	}
+
+}
---
+++ apecs/src/test/java/com/apecs/factura/apecs/ApecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apecs.factura.apecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
