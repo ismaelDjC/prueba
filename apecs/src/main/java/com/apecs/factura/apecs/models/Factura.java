e15a36a3d42e0039d04435774b862f5f2cd01231
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-04-28 16:40:37 -0500

    nuevos camvios v2

--- apecs/src/main/java/com/apecs/factura/apecs/models/Factura.java
+++ apecs/src/main/java/com/apecs/factura/apecs/models/Factura.java
@@ -1,5 +1,47 @@
 package com.apecs.factura.apecs.models;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "factura")
 public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id_secretaria;
+
+    private Date fecha;
+
+    private boolean total;
+
+    public Factura(Date fecha, boolean total) {
+        this.fecha = fecha;
+        this.total = total;
+    }
+    public Factura(){}
+    public Integer getId_secretaria() {
+        return Id_secretaria;
+    }
+    public void setId_secretaria(Integer id_secretaria) {
+        Id_secretaria = id_secretaria;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public boolean isTotal() {
+        return total;
+    }
+    public void setTotal(boolean total) {
+        this.total = total;
+    }
+
     
 }
--- apecs/src/main/java/com/apecs/factura/apecs/repository/Factura_Repository.java
+++ apecs/src/main/java/com/apecs/factura/apecs/repository/Factura_Repository.java
@@ -1,5 +1,9 @@
 package com.apecs.factura.apecs.repository;
 
-public class Factura_Repository {
+import com.apecs.factura.apecs.models.Factura;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public  interface Factura_Repository extends JpaRepository<Factura, Integer> {
     
 }
