8d8325705bb687abb8fb4b495fd92541095ad717
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-04-28 22:05:59 -0500

    nuevos cambios

--- apecs/src/main/java/com/apecs/factura/apecs/controller/DetalleController.java
+++ apecs/src/main/java/com/apecs/factura/apecs/controller/DetalleController.java
@@ -1,5 +1,41 @@
 package com.apecs.factura.apecs.controller;
+import java.util.List;
 
+import com.apecs.factura.apecs.models.Detalle;
+import com.apecs.factura.apecs.repository.Detalle_Repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+		RequestMethod.DELETE })
+@RequestMapping("/api/detalle")
 public class DetalleController {
+
+    @Autowired
+	private Detalle_Repository repository;
+
+    @GetMapping("/")
+	public List<Detalle> readAll() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/inventory")
+	public Detalle create(@Validated @RequestBody Detalle l) {
+		return repository.save(l);
+	}
+
+	@PutMapping("/inventory/{id}")
+	public Detalle update(@PathVariable Integer id, @Validated @RequestBody Detalle l) {
+		return repository.save(l);
+	}
+
+	@DeleteMapping("/inventory/{id}")
+	public void delete(@PathVariable Integer id) {
+		repository.deleteById(id);
+	}
     
 }
--- apecs/src/main/java/com/apecs/factura/apecs/controller/FacturaController.java
+++ apecs/src/main/java/com/apecs/factura/apecs/controller/FacturaController.java
@@ -1,5 +1,45 @@
 package com.apecs.factura.apecs.controller;
 
+import java.util.List;
+
+import com.apecs.factura.apecs.models.Factura;
+import com.apecs.factura.apecs.repository.Factura_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+		RequestMethod.DELETE })
+@RequestMapping("/api/factura")
 public class FacturaController {
+
+    @Autowired
+    private Factura_Repository  repository;
+
+    @GetMapping("/")
+	public List<Factura> readAll() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/factura")
+	public Factura create(@Validated @RequestBody Factura l) {
+		return repository.save(l);
+	}
+
+	@PutMapping("/factura/{id}")
+	public Factura update(@PathVariable Integer id, @Validated @RequestBody Factura l) {
+		return repository.save(l);
+	}
+
+	@DeleteMapping("/factura/{id}")
+	public void delete(@PathVariable Integer id) {
+		repository.deleteById(id);
+	}
+
+
+
+    
     
 }
